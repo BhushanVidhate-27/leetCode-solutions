fa7acba4aaa9031ec72245c92dcb6261771d0f90
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-05-19 00:50:20 +0530

    Add solution for Bulb Switcher

---
+++ solutions/medium/Bulb Switcher.cpp
@@ -0,0 +1,12 @@
+// Title: Bulb Switcher
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/bulb-switcher/
+
+using namespace std;
+public:
+    int bulbSwitch(int n) {
+        return floor(sqrt(n));
+    }
+class Solution {
+};
