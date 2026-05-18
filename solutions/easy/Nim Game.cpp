5f29cf8e468ed66bc7a952ca5f5eb057c08b34e2
Bhushan Vidhate <bhushanvidhate109@gmail.com>
2026-05-19 00:45:40 +0530

    Add solution for Nim Game

---
+++ solutions/easy/Nim Game.cpp
@@ -0,0 +1,11 @@
+// Title: Nim Game
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/nim-game/
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n%4 != 0;
+    }
+};
